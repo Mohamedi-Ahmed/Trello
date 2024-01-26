b8cfe9d0d8f455bb0dd8608826cfa0ca34a68dd3
Ahmed_M <62441252+Mohamedi-Ahmed@users.noreply.github.com>
2024-01-26 13:27:05 +0100

    final project

--- Backend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TrelloMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './Components/app/app.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  },
-];
